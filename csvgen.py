166e8ecf9f1dd1447b6c30379b312262d157b570
Hari Shanmugaraja <harishanmugaraja@gmail.com>
2019-08-16 15:37:58 -0400

    updated the csvgen to match the ds and y headers for prophet

--- csvgen.py
+++ csvgen.py
@@ -1,11 +1,14 @@
+#Hari Shanmugaraja      08/16/19
 import csv
 import random
+import datetime
 
 with open("expenses.csv","w+",newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["Expenses"])
+    writer.writerow(["ds","y"])
     numweeks = 30
     temprr = []
+    date = datetime.datetime(2019,8,19)
     for x in range(1,numweeks*7+1):
         dailyex = random.uniform(250,300)
         if x%7==2:
@@ -16,4 +19,5 @@ with open("expenses.csv","w+",newline="") as f:
             dailyex+=25
         if x%10==0:
             dailyex+=30
-        writer.writerow([dailyex])
+        writer.writerow([date,dailyex])
+        date += datetime.timedelta(days=1)
